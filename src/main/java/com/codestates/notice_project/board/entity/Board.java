def04f6248b57243668aba8ab2a277be0331117e
Bhinney <snakellie97@gmail.com>
2022-10-15 21:16:53 +0900

    service, mapper 구현

--- src/main/java/com/codestates/notice_project/board/entity/Board.java
+++ src/main/java/com/codestates/notice_project/board/entity/Board.java
@@ -4,14 +4,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long boardId;
+
+    @Column(length = 100, nullable = false)
     private String writer;
+
+    @NotBlank
     private String comment;
-    private BoardStatus boardStatus;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 50, nullable = false)
+    private BoardStatus boardStatus = BoardStatus.BOARD_PUBLIC;
 
     public Board(long boardId, String writer, String comment) {
         this.boardId = boardId;
@@ -24,6 +38,7 @@ public class Board {
         BOARD_PRIVATELY("게시물 비공개"),
         BOARD_DELETE("게시물 삭제");
 
+        @Getter
         private String status;
 
         BoardStatus(String status){this.status = status;}
